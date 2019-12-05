c4f582d887969ce37657dc67c5e8ee3370320816
Alistair Rowden <deadlyhardware@gmail.com>
2019-12-05 14:43:37 -0500

    Client side data is logged to the console

--- assets/javascript/app2.js
+++ assets/javascript/app2.js
@@ -2,42 +2,42 @@ $(document).ready();
 
 let dataArray = [];
 
-        $(function() {
-            let carMake = $('#addCarMake');
-            let mileage = $("#addCurrentMileage");
-            let interval  = $("#addOilChangeInterval");
-            let submit = $('#submit');
+let carMake = $('#addCarMake');
+let mileage = $("#addCurrentMileage");
+let interval  = $("#addOilChangeInterval");
+
+    $('#submit').on('click', function(event){ 
+        event.preventDefault();
+            dataObj = {
+                'Make'    : carMake.val().trim(),
+                'Mileage' : mileage.val().trim(),
+                'Interval': interval.val().trim()
+            };
+            let carMakeAndModel = dataObj.Make;
+            let currentMileage = dataObj.Mileage;
+            let nextInterval = dataObj.Interval;
             
-            submit.on('click', function(event){ 
-                event.preventDefault();
-                    dataObj = {
-                        'Make'    : carMake.val(),
-                        'Mileage' : mileage.val(),
-                        'Interval': interval.val()
-                    };
-                    let carMakeAndModel = dataObj.Make;
-                    let currentMileage = dataObj.Mileage;
-                    let nextInterval = dataObj.Interval;
-                    
-                    console.log(`Make and model: ${carMakeAndModel}`);
-                    console.log(`Current mileage: ${currentMileage} miles`);
-                    console.log(`Oil change interval: ${nextInterval} miles`);
-                    console.log(`Next oil change is due at: ${nextServiceDue(currentMileage, nextInterval)} miles`);
-                    dataArray.push(dataObj);
+            console.log(`Make and model: ${carMakeAndModel}`);
+            console.log(`Current mileage: ${currentMileage} miles`);
+            console.log(`Oil change interval: ${nextInterval} miles`);
+            console.log(`Next oil change is due at: ${nextServiceDue(currentMileage, nextInterval)} miles`);
+            dataArray.push(dataObj);
 
-                    localStorage.setItem('dataArray', JSON.stringify(dataArray));
-            });
+            localStorage.setItem('dataArray', JSON.stringify(dataArray));
 
-            retrivedArray = localStorage.getItem('dataArray');     
-            console.log('retrivedArray: ', JSON.parse(retrivedArray));
+    });
 
-        
-            // function to work out next service interval
-            function nextServiceDue(currentMileage, nextInterval) {
-                return parseInt(currentMileage) + parseInt(nextInterval);
-            }
+    let retrivedArray = localStorage.getItem('dataArray');     
+    let clientStorage = JSON.parse(retrivedArray);
+    for (let i = 0; i < clientStorage.length; i++) {
+        console.log('retrivedArray: ', clientStorage[i]);
+    }
 
-            });
+
+    // function to work out next service interval
+    function nextServiceDue(currentMileage, nextInterval) {
+        return parseInt(currentMileage) + parseInt(nextInterval);
+    }
 
 
 
