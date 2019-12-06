aa46591f6ace92628ffc411dee3168bfc7fd517d
Alistair Rowden <deadlyhardware@gmail.com>
2019-12-05 23:28:53 -0500

    Style cards with sass

--- assets/javascript/app2.js
+++ assets/javascript/app2.js
@@ -2,26 +2,17 @@ $(document).ready();
 
 let carArray = [];
 
-let carMake = $('#addCarMake');
-let mileage = $("#addCurrentMileage");
-let interval  = $("#addOilChangeInterval");
-
     $('#submit').on('click', function(event){ 
         // event.preventDefault();
+        let carMake = $('#addCarMake');
+        let mileage = $("#addCurrentMileage");
+        let interval  = $("#addOilChangeInterval");
+
             dataObj = {
                 'Make'    : carMake.val().trim(),
                 'Mileage' : mileage.val().trim(),
                 'Interval': interval.val().trim()
             };
-            // let carMakeAndModel = dataObj.Make;
-            // let currentMileage = dataObj.Mileage;
-            // let nextInterval = dataObj.Interval;
-            
-            // console.log(`Make and model: ${carMakeAndModel}`);
-            // console.log(`Current mileage: ${currentMileage} miles`);
-            // console.log(`Oil change interval: ${nextInterval} miles`);
-            // console.log(`Next oil change is due at: ${nextServiceDue(currentMileage, nextInterval)} miles`);
-            
             carArray.push(dataObj);
             localStorage.setItem('carArray', JSON.stringify(carArray));
 
@@ -32,19 +23,20 @@ let interval  = $("#addOilChangeInterval");
     });
 
     // Renders vehicle data to the page
-    let existingData = JSON.parse(localStorage.getItem('carArray'));
-    for (let i = 0; i < existingData.length; i++) {
-        // console.log('existingData: ', existingData[i]);
+    let vehicle = JSON.parse(localStorage.getItem('carArray'));
+    for (let i = 0; i < vehicle.length; i++) {
+        // console.log('vehicle: ', vehicle[i]);
         $('#carInfo').append(`
-            <div class='col-xs-4 p-2 mx-auto'>
-                <div class="card" style="width: 18rem;">
+            <div class='col-xs-4 p-2 mt-3 mx-auto'>
+                <div class="card rounded-0" style="width: 18rem;">
                         <div class="card-body">
-                            <h5 class="card-title">${existingData[i].Make}</h5>
+                            <h5 class="card-title">${vehicle[i].Make}</h5>
                         </div>
                     <ul class="list-group list-group-flush">
                         <li class="list-group-item"></li>
-                        <li class="list-group-item"><h6>Mileage:</h6> ${existingData[i].Mileage} miles</li>
-                        <li class="list-group-item"><h6>Oil Change Interval:</h6> ${existingData[i].Interval} miles</li>
+                        <li class="list-group-item"><h6>Mileage:</h6>${vehicle[i].Mileage} miles</li>
+                        <li class="list-group-item"><h6>Oil Change Interval:</h6>${vehicle[i].Interval} miles</li>
+                        <li class="list-group-item"><h6>Oil Change Due:</h6>${nextServiceDue(vehicle[i].Mileage, vehicle[i].Interval)} miles</li>
                     </ul>
                     <div class="card-body">
                         <a href="#" class="card-link">Help</a>
@@ -56,6 +48,6 @@ let interval  = $("#addOilChangeInterval");
     }
 
     // function to work out next service interval
-    // function nextServiceDue(currentMileage, nextInterval) {
-    //     return parseInt(currentMileage) + parseInt(nextInterval);
-    // }
+    function nextServiceDue(currentMileage, nextInterval) {
+        return parseInt(currentMileage) + parseInt(nextInterval);
+    }
