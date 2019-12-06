7f2aa94437ee470c53dabd222701b138bac64860
Alistair Rowden <deadlyhardware@gmail.com>
2019-12-05 23:38:28 -0500

    Tweak UI/UX

--- assets/javascript/app2.js
+++ assets/javascript/app2.js
@@ -22,10 +22,9 @@ let carArray = [];
             localStorage.setItem('carArray', JSON.stringify(existingData));
     });
 
-    // Renders vehicle data to the page
+    // Renders vehicle data to the page via Bootstrap cards
     let vehicle = JSON.parse(localStorage.getItem('carArray'));
     for (let i = 0; i < vehicle.length; i++) {
-        // console.log('vehicle: ', vehicle[i]);
         $('#carInfo').append(`
             <div class='col-xs-4 p-2 mt-3 mx-auto'>
                 <div class="card rounded-0" style="width: 18rem;">
@@ -34,13 +33,13 @@ let carArray = [];
                         </div>
                     <ul class="list-group list-group-flush">
                         <li class="list-group-item"></li>
-                        <li class="list-group-item"><h6>Mileage:</h6>${vehicle[i].Mileage} miles</li>
-                        <li class="list-group-item"><h6>Oil Change Interval:</h6>${vehicle[i].Interval} miles</li>
-                        <li class="list-group-item"><h6>Oil Change Due:</h6>${nextServiceDue(vehicle[i].Mileage, vehicle[i].Interval)} miles</li>
+                        <li class="list-group-item text-left"><h6>Mileage:</h6>${vehicle[i].Mileage} miles</li>
+                        <li class="list-group-item text-left"><h6>Oil Change Interval:</h6>${vehicle[i].Interval} miles</li>
+                        <li class="list-group-item text-left"><h6>Oil Change Due:</h6>${nextServiceDue(vehicle[i].Mileage, vehicle[i].Interval)} miles</li>
                     </ul>
                     <div class="card-body">
                         <a href="#" class="card-link">Help</a>
-                        <a href="#" class="card-link">Info</a>
+                        <a href="#" class="card-link">More Service Data</a>
                     </div>
                 </div>
             </div>
